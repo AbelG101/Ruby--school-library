f6857757c6e539917afe61db42c3870574bcb398
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-01 20:36:47 +0300

    Add trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,10 @@
+require_relative './base_decorator'
+require_relative './person'
+require_relative './capitalize_decorator'
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    return @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
+    @nameable.correct_name
+  end
+end
