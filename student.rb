c9b565c04fee538a20dfc50ea409b3e388531971
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-02 11:32:54 +0300

    Add student classroom relation

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permmission: true)
     super(age, name, parent_permmission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
