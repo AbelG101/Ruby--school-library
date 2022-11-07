de49ed863fc6019bf39db28bf95f463f45e8b2a7
whid-bb <whid11s50@gmail.com>
2022-11-07 14:46:45 +0100

    Refactor to class

--- main.rb
+++ main.rb
@@ -1,40 +1,43 @@
 require_relative './app'
 
-def prompt
-  loop do
-    display_options
-    option = take_input
-    option_methods(option)
-
-    break if option == 7
+class Main
+  def initialize
+    @app = App.new
+    prompt
   end
-end
 
-def display_options
-  puts 'Please choose one of the options: '
-  puts '1. - List all books'
-  puts '2. - List all people'
-  puts '3. - Create a person'
-  puts '4. - Create a book'
-  puts '5. - Create a rental'
-  puts '6. - List all rentals for a given person ID'
-  puts '7. - Exit'
-end
+  def prompt
+    loop do
+      display_options
+      option = take_input
 
-def take_input
-  print 'Enter a number: '
-  option = gets.chomp.to_i
-  while option.nil? || option < 1 || option > 7
-    puts 'Please enter a number between 1 and 7!'
+      @app.run(option)
+
+      break if option == 7
+    end
+  end
+
+  def display_options
+    puts 'Please choose one of the options: '
+    puts '1. - List all books'
+    puts '2. - List all people'
+    puts '3. - Create a person'
+    puts '4. - Create a book'
+    puts '5. - Create a rental'
+    puts '6. - List all rentals for a given person ID'
+    puts '7. - Exit'
+  end
+
+  def take_input
     print 'Enter a number: '
     option = gets.chomp.to_i
+    while option.nil? || option < 1 || option > 7
+      puts 'Please enter a number between 1 and 7!'
+      print 'Enter a number: '
+      option = gets.chomp.to_i
+    end
+    option
   end
-  option
 end
 
-def main
-  app = App.new
-  app.run
-end
-
-main
+Main.new
