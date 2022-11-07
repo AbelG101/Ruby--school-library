a695e5bcdbc9cf7c5e9e807bb507c9542b5e1ec8
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-07 18:02:43 +0300

    Update App class by migrating previous functions to separate class

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ require_relative './teacher'
 require_relative './book'
 require_relative './classroom'
 require_relative './rental'
-# require_relative './student'
-require_relative './option_methods'
-class App
-  attr_accessor :books, :students, :teachers, :rentals
+require_relative './create_book'
+require_relative './create_rental'
+require_relative './create_person'
 
-  include OptionMethods
+class App
+  include CreateBook
+  include CreateRental
+  include CreatePerson
+
+  attr_accessor :books, :students, :teachers, :rentals
 
   def initialize
     @books = []
@@ -28,63 +32,6 @@ class App
     end
   end
 
-  def create_person
-    print 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
-    person_option = gets.chomp.to_i
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    if person_option == 1
-      print 'Classroom: '
-      classroom_ = gets.chomp
-      print 'Has parent permmission? [Y/N]: '
-      parent_permmission = gets.chomp.downcase == 'y'
-      create_student(name, age, parent_permmission, classroom_)
-    else
-      print 'Specialization: '
-      specialization = gets.chomp
-      create_teacher(name, age, specialization)
-    end
-    puts 'Person created successfully'
-  end
-
-  def create_student(name, age, parent_permmission, classroom_)
-    classroom = Classroom.new(classroom_)
-    student = Student.new(classroom, age, name: name, parent_permmission: parent_permmission)
-    @people << student
-  end
-
-  def create_teacher(name, age, specialization)
-    teacher = Teacher.new(specialization, age, name)
-    @people << teacher
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @books << book
-    puts 'Book created successfully'
-  end
-
-  def create_rental
-    puts 'Select a book from the following list by number'
-    display_all_books
-    selected_book_option = gets.chomp.to_i
-    display_all_people
-    selected_person_option = gets.chomp.to_i
-    print 'Date: '
-    date = gets.chomp
-    book = @books[selected_book_option]
-    person = @people[selected_person_option]
-    rental = Rental.new(date, book, person)
-    @rentals << rental
-    puts 'Rental created successfully'
-  end
-
   def list_rental_by_id
     print 'Id of person: '
     id = gets.chomp.to_i
@@ -94,6 +41,22 @@ class App
   end
 
   def run(option)
-    option_methods(option)
+    case option
+    when 1
+      display_all_books
+    when 2
+      display_all_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rental_by_id
+    else
+      puts 'Thank you for using this app!'
+      exit
+    end
   end
 end
