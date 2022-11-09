dcfdb1aa805899280b65a5558a4fc1bc55fab1a0
whid-bb <whid11s50@gmail.com>
2022-11-09 17:05:53 +0100

    Fix 10 chars error

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -4,7 +4,7 @@ require_relative './capitalize_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name[0, 9] if @nameable.correct_name.length > 10
+    return @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
 
     @nameable.correct_name
   end
